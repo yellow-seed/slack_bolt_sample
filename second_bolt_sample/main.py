1118ca7ad75523a8acef3e1616a38fb2094124f9
KeisukeIto <a2201ki@aiit.ac.jp>
2023-05-24 21:40:29 +0900

    Hello 出してみた

--- second_bolt_sample/main.py
+++ second_bolt_sample/main.py
@@ -1,9 +1,20 @@
 import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from dotenv import load_dotenv
+
+# 環境変数を設定
+load_dotenv()
 
 # ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+
+@app.message("hello")
+def message_hello(message, say):
+    # イベントがトリガーされたチャンネルへ say() でメッセージを送信します
+    say(f"Hey there <@{message['user']}>!")
+
 
 # アプリを起動します
 if __name__ == "__main__":
