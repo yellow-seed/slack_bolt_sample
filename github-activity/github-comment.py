da55dd143711ab0150cbc552a8121a4e4d81f31c
me_norin <a2223ht@aiit.ac.jp>
2023-06-20 10:12:04 +0900

    Updateがあるかどうかチェックする関数を追加することで動作を高速化した

--- github-activity/github-comment.py
+++ github-activity/github-comment.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-import os,sys
+import os, sys
+
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../notion_db_sample")
 
 from rich import print
@@ -12,12 +13,59 @@ from time import sleep
 
 from pynotion import DbContents
 
+
 def countComment(comment_users, login_name):
+    """
+    Comment数をCountする。
+
+    Args:
+        comment_users (list): Count対象のユーザ名が記されているlist
+        login_name (str): listに含まれているCountしたいユーザ名
+
+    Returns:
+        int: number of count
+    """
     ret_count = comment_users.count(login_name)
     if ret_count == 0:
         ret_count = None
-    
+
     return ret_count
+
+
+def isPullReqUpdated(notion_rows: dict, github_target: dict) -> bool:
+    """
+        GithubのあるPRのコメント数が更新されているかチェックする
+
+    Args:
+        notion_rows (dict): Notion側の情報
+        github_target (dict): Github側の情報
+
+    Returns:
+        bool: True 更新されている/新規のPRである。 False 更新されていない
+    """
+    result = False
+    pr_num = github_target["No."]
+    for notion_row in notion_rows:
+        if pr_num == notion_row["No."]:
+            break
+    else:
+        # No.該当なし。つまり新しいPRである。
+        return True
+
+    for key, notion_value in notion_row.items():
+        if type(notion_value) == list:
+            if github_target[key] != notion_value[0]:
+                result = True
+                break
+        else:  # type(notion_value) != list
+            if github_target[key] != notion_value:
+                result = True
+                break
+    else:
+        result = False
+
+    return result
+
 
 if __name__ == "__main__":
     # 情報をロード
@@ -39,9 +87,7 @@ if __name__ == "__main__":
     # Notion DBオブジェクト作成
     db_contents = DbContents(notion_api_key)
     # データを取得
-    rows = db_contents.pull(table_id)
-    #print(rows[0])
-
+    notion_rows = db_contents.pull(table_id)
 
     # ここからGithubのAPIを使用したサンプル
     # Public Web Github
@@ -52,48 +98,42 @@ if __name__ == "__main__":
 
     for pr_obj in pr_objs:
         print("#{0} Title: {1}, 作成者: {2}".format(pr_obj.number, pr_obj.title, pr_obj.user.login))
-        
-        create_date = "{0}".format(pr_obj.created_at).split(" ")[0]     #例えば、2023-05-19 14:59:40の日付部分2023-05-19を抽出
+
+        create_date = "{0}".format(pr_obj.created_at).split(" ")[0]  # 例えば、2023-05-19 14:59:40の日付部分2023-05-19を抽出
         pr_status = None
 
         if pr_obj.merged_at != None:
-            pr_status = 'Merged'
+            pr_status = "Merged"
         else:
-            if pr_obj.state == 'closed':
-                pr_status = 'Closed'
+            if pr_obj.state == "closed":
+                pr_status = "Closed"
             else:
-                pr_status = 'Open'
+                pr_status = "Open"
 
         comment_users = [pr_obj.user.login]
         comments = pr_obj.get_issue_comments()
         for comment in comments:
             comment_users.append(comment.user.login)
-        
-        sleep(0.5)
-        db_contents.delete("No.", pr_obj.number)
 
-        # データを追加
-        sleep(0.5)
-        row_dict = {
-                        'No.': pr_obj.number,
-                        'タイトル': pr_obj.title,
-                        '作成者': pr_obj.user.login,
-                        tanaka_login_name : countComment(comment_users, tanaka_login_name),
-                        baba_login_name : countComment(comment_users, baba_login_name),
-                        ito_login_name : countComment(comment_users, ito_login_name),
-                        nakagawa_login_name : countComment(comment_users, nakagawa_login_name),
-                        uemura_login_name : countComment(comment_users, uemura_login_name),
-                        nakauchi_login_name : countComment(comment_users, nakauchi_login_name),
-                        iwama_login_name : countComment(comment_users, iwama_login_name),
-                        '作成日': create_date,
-                        'ステータス': pr_status,
-                        'URL': pr_obj.html_url
-                    }
-        db_contents.append(row_dict)
+        pr_dict = {
+            "No.": pr_obj.number,
+            "タイトル": pr_obj.title,
+            "作成者": pr_obj.user.login,
+            tanaka_login_name: countComment(comment_users, tanaka_login_name),
+            baba_login_name: countComment(comment_users, baba_login_name),
+            ito_login_name: countComment(comment_users, ito_login_name),
+            nakagawa_login_name: countComment(comment_users, nakagawa_login_name),
+            uemura_login_name: countComment(comment_users, uemura_login_name),
+            nakauchi_login_name: countComment(comment_users, nakauchi_login_name),
+            iwama_login_name: countComment(comment_users, iwama_login_name),
+            "作成日": create_date,
+            "ステータス": pr_status,
+            "URL": pr_obj.html_url,
+        }
+        if isPullReqUpdated(notion_rows, pr_dict) == True:
+            sleep(0.5)
+            db_contents.delete("No.", pr_obj.number)
 
-
-
-
-
-
-
+            # データを追加
+            sleep(0.5)
+            db_contents.append(pr_dict)
