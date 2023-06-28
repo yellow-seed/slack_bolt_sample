e0180e5c77082cbf4ce09cb0bb1f843ae1f4d3e8
me_norin <a2223ht@aiit.ac.jp>
2023-06-28 23:46:01 +0900

    動作確認する環境を整えた

--- api-data-struct/main.py
+++ api-data-struct/main.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     # 実装した構造化スクリプト
     import notion_st
 
-    INDEX_ZERO = 0
+    table_row = 0
     database_id = "01be2b6ddec849d199e6c4f555accc98"
 
     # 秘密情報をロード
@@ -24,17 +24,40 @@ if __name__ == "__main__":
     api_key = os.environ.get("NOTION_API_KEY")
     notion = Client(auth=api_key)
 
+    # notionからのResponse情報を得る
     response_dict = notion.databases.query(**{"database_id": database_id})
+
+    # Responseをパースする
     db_query_res = notion_st.purseDbQueryRes(response_dict)
-    print(db_query_res.results[0].archived)
 
-    user_id = notion_st.pursePageTitle(db_query_res.results[0].properties["userid"])
-    quarter_num = notion_st.pursePageText(db_query_res.results[0].properties["活動報告"])
-    tag = notion_st.pursePageMultiSelect(db_query_res.results[0].properties["タグ"])
-    contents = notion_st.pursePageText(db_query_res.results[0].properties["内容"])
+    # Responseの中身のpropertiesをパースする
+    # purseDbQueryResですべてパースできないのは、Notionに置いてある表の情報を教えてあげる必要があるため。
+    # 列ごとに、ヘッダー名とtypeが異なる。その情報をパーサーに入力する必要がある
+    user_id = notion_st.pursePageTitle(db_query_res.results[table_row].properties["userid"])  # typeがTitleのプロパティをパース
+    quarter_num = notion_st.pursePageText(db_query_res.results[table_row].properties["活動報告"])  # typeがTextのプロパティをパース
+    tag = notion_st.pursePageMultiSelect(db_query_res.results[table_row].properties["タグ"])  # typeがMultiSelectのプロパティをパース
+    contents = notion_st.pursePageText(db_query_res.results[table_row].properties["内容"])  # typeがTextのプロパティをパース
 
-    print(user_id.title[INDEX_ZERO].text.content)
-    print(tag.multi_select[INDEX_ZERO])
-    print(tag.multi_select[INDEX_ZERO].name)
+    # typeに関しては自動識別可能なパーサーも別途準備した。
+    # ただし、返り値がUnionになるので型ヒントとして不要な要素もサジェストされる
+    tag_union = notion_st.pursePagePropaty(db_query_res.results[table_row].properties["タグ"])
 
-    tag = notion_st.pursePagePropaty(db_query_res.results[0].properties["タグ"])
+    """
+        表記例
+    """
+    print(db_query_res.results[table_row].last_edited_by)
+    print(user_id.title[0].text.content)
+    print(tag.multi_select[0])
+    print(quarter_num.rich_text[0].text)
+    print(tag_union.multi_select[0].name)
+    print('\n"-------ここから動作確認-------"')
+    """
+        下記の変数についてVS CODEの型ヒントが利くことを動作確認してください。
+    """
+
+    db_query_res
+    user_id
+    quarter_num
+    contents
+    tag
+    tag_union
