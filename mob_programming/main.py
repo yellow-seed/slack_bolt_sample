a86bcdec0aa0ca2fd365b1ce65243a55498d7a2e
Chorinu <you@example.com>
2023-07-05 22:00:34 +0900

    Flaskでslack_eventsを追加

--- mob_programming/main.py
+++ mob_programming/main.py
@@ -13,6 +13,23 @@ from rich import print
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+from flask import Flask, request
+from slack_bolt.adapter.flask import SlackRequestHandler
+
+flask_app = Flask(__name__)
+handler = SlackRequestHandler(app)
+
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    payload = request.get_json()
+    if 'challenge' in payload:
+        # これはチャレンジリクエストです。アプリを確認するためにチャレンジ値で応答してください。
+        # ref: https://qiita.com/masa_masa_ra/items/618779e698921cb53cec
+        return payload['challenge']
+    else:
+        # これは通常のイベントです。SlackRequestHandlerで処理してください。
+        return handler.handle(request)
+    
 # 環境変数を設定
 load_dotenv()
 # os.environ["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY")
@@ -196,7 +213,7 @@ def fetch_records_for_week(semester=None, week=None):
 
     return results
 
-
+# MOB flask_app.runで書き換え
 # アプリを起動します
 if __name__ == "__main__":
     mention_text = (
