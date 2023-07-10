b90081a10acbf31f585b622d87562d1578140bf5
me_norin <a2223ht@aiit.ac.jp>
2023-07-11 00:05:52 +0900

    Request URLをngrokから自動で取得してSlack Appに設定する。Slack AppのConfig Tokenが切れた場合は自動で更新する。

--- flask-ngrok-sample/flask_app.py
+++ flask-ngrok-sample/flask_app.py
@@ -1,24 +1,52 @@
 #!/usr/bin/env python3
 
-import sys, os
+import sys
+import os
+from flask import Flask, request
+import ngrok
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-from flask import Flask, request
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../slack-app-manifest")
 
 
 def initFlaskApp(flask_app):
     @flask_app.route("/slack/events", methods=["POST"])
     def slack_events():
+        """
+        Slackからのイベントを受け取る
+        チャレンジリクエストと通常のイベントを区別して処理する。
+        チャレンジリクエストの処理は、下記ページを参考にした。
+        https://qiita.com/masa_masa_ra/items/618779e698921cb53cec
+
+        Returns:
+            dict: Slackからのレスポンス
+        """
         payload = request.get_json()
         if "challenge" in payload:
             # これはチャレンジリクエストです。アプリを確認するためにチャレンジ値で応答してください。
-            # ref: https://qiita.com/masa_masa_ra/items/618779e698921cb53cec
             return payload["challenge"]
         else:
             # これは通常のイベントです。SlackRequestHandlerで処理してください。
             return handler.handle(request)
         return handler.handle(request)
+
+
+def getNgrokUrl(ngrok_api_key) -> str:
+    """
+    ngrokのURLを取得する
+    https://python-api.docs.ngrok.com/client.html#ngrok.Client.tunnels
+
+    Args:
+        ngrok_api_key (str): ngrokのAPIキー
+
+    Returns:
+        str: ngrokのURL
+    """
+    ngrok_client = ngrok.Client(ngrok_api_key)
+    for tunnel in ngrok_client.tunnels.list():
+        print("ngrok URL -> ", tunnel.public_url)
+
+    return tunnel.public_url
 
 
 if __name__ == "__main__":
@@ -27,17 +55,33 @@ if __name__ == "__main__":
     from slack_bolt import App
     from slack_bolt.adapter.flask import SlackRequestHandler
 
-    from app_handler import SlackAppController  # 自作クラス
+    # 自作クラス
+    from app_handler import SlackAppController
+    from create_app import Manifest
 
     # 設定情報をロード
     config = ConfigParser()
-    config.read("config.ini", encoding="utf-8")
+    config.read("./flask-ngrok-sample/config.ini", encoding="utf-8")
     # 秘密情報をロード
     load_dotenv()
 
     # ロードした情報を格納
     access_token = os.environ.get("SLACK_BOT_TOKEN")
     signing_secret = os.environ.get("SLACK_SIGNING_SECRET")
+    ngrok_api_key = os.environ.get("NGROK_API_KEY")
+    slack_app_id = config.get("Slack", "app_id")
+    slacK_conf_refresh_token = os.environ.get("SLACK_CONF_REFRESH_TOKEN")
+    slack_conf_token = os.environ.get("SLACK_CONF_TOKEN")
+
+    # ngrokのURLを取得
+    ngrok_url = getNgrokUrl(ngrok_api_key)
+    slack_request_url = ngrok_url + "/slack/events"
+
+    # Slack Botの設定
+    manifest = Manifest(slacK_conf_refresh_token, slack_conf_token)
+    manifest.getFileFromApp(slack_app_id)
+    manifest.switchSocketModeAndEventSubsc(slack_app_id, socket_mode_enabled=False, request_url=slack_request_url)
+    manifest.changeAppConfig(slack_app_id)
 
     # インスタンス生成
     slack_app = App(signing_secret=signing_secret, token=access_token)
--- slack-app-manifest/create_app.py
+++ slack-app-manifest/create_app.py
@@ -9,8 +9,77 @@ import requests
 import json
 
 
+class SlackToken(object):
+    """
+    
+    """
+    def __init__(self, refresh_token, config_token=None) -> None:
+        self.refresh = refresh_token
+        self.config = config_token
+        self.base_url = "https://slack.com/api/"
+
+    def refreshConfigToken(self) -> str:
+        """
+        Refresh Tokenを使ってConfig Tokenを取得する
+        ※期限切れ前の古いConfig Tokenも引き続き使用可能である。
+        https://api.slack.com/methods/tooling.tokens.rotate
+
+        """
+        rotate_api = self.base_url + "tooling.tokens.rotate"
+        post_data = {"refresh_token": self.refresh}
+        print(self.refresh)
+        response = requests.post(rotate_api, post_data)
+        if response.json()["ok"] is True:
+            # 環境変数のCONF_TOKENを更新する
+            self.config = response.json()["token"]
+            self.refresh = response.json()["refresh_token"]
+            self.issue_at = response.json()["iat"]
+            self.expires_in = response.json()["exp"]
+            env_config = ConfigParser()
+            env_config.optionxform = str
+            env_config.read("./.env", encoding="utf-8")
+            env_config.set("Env", "SLACK_CONF_TOKEN", self.config)
+            env_config.set("Env", "SLACK_CONF_REFRESH_TOKEN", self.refresh)
+
+            # 実際のINIファイルに反映させる
+            with open("./.env", "w") as f:
+                env_config.write(f)
+
+            # 環境変数を更新する
+            print("Config Token & Refresg Token is updated in .evn file。\nDev ContainerをRebuildしてください。")
+
+        else:
+            print(response.json())
+            self.config = None
+
+        return self.config
+
+    def checkConfigToken(self) -> bool:
+        """
+        Config Tokenが有効かどうかをチェックする。
+        https://api.slack.com/methods/auth.test
+
+        Returns:
+            bool: True:有効, False:無効
+        """
+        auth_api = self.base_url + "auth.test"
+        post_data = {"token": self.config}
+        response = requests.post(auth_api, post_data)
+
+        ret = response.json()["ok"]
+        if ret is True:
+            # トークンは有効である。
+            print("Config Token is valid.")
+            return True
+        else:
+            # トークンは下記の理由で無効である。
+            print("checkConfigToken result: ", response.json()["error"])
+            print("Your ConfigToken: ", self.config)
+            return False
+
+
 class Manifest(object):
-    def __init__(self, token):
+    def __init__(self, refresh_token, config_token=None):
         """
         初期化関数
 
@@ -18,7 +87,14 @@ class Manifest(object):
             token (str): Authentication token bearing required scopes.
                          Tokens should be passed as an HTTP Authorization header or alternatively, as a POST parameter.
         """
-        self.token = token
+        self.token = SlackToken(refresh_token, config_token)
+        if self.token.checkConfigToken() is False:
+            self.config_token = self.token.refreshConfigToken()
+        else:
+            self.config_token = config_token
+
+        self.issue_at = None
+        self.expires_in = None
         self.base_url = "https://slack.com/api/"
         self.manifest_config = None
 
@@ -31,7 +107,7 @@ class Manifest(object):
             app_id (str): The ID of the app whose configuration you want to export as a manifest.
         """
         export_api = self.base_url + "apps.manifest.export"
-        post_data = {"token": self.token, "app_id": app_id}
+        post_data = {"token": self.config_token, "app_id": app_id}
         response = requests.post(export_api, post_data)
         if self.__checkResponse(response, "getFileFromApp") is True:
             self.manifest_config = response.json()["manifest"]
@@ -63,7 +139,7 @@ class Manifest(object):
         https://api.slack.com/methods/apps.manifest.create
         """
         create_api = self.base_url + "apps.manifest.create"
-        post_data = {"token": self.token, "manifest": json.dumps(self.manifest_config)}
+        post_data = {"token": self.config_token, "manifest": json.dumps(self.manifest_config)}
         response = requests.post(create_api, post_data)
         self.__checkResponse(response, "createApp")
 
@@ -76,7 +152,7 @@ class Manifest(object):
             app_id (str): The ID of the app whose configuration you want to export as a manifest.
         """
         delete_api = self.base_url + "apps.manifest.delete"
-        post_data = {"token": self.token, "app_id": app_id}
+        post_data = {"token": self.config_token, "app_id": app_id}
         response = requests.post(delete_api, post_data)
         self.__checkResponse(response, "deleteApp")
 
@@ -90,9 +166,27 @@ class Manifest(object):
             app_id (str): The ID of the app whose configuration you want to export as a manifest.
         """
         update_api = self.base_url + "apps.manifest.update"
-        post_data = {"token": self.token, "app_id": app_id, "manifest": json.dumps(self.manifest_config)}
+        post_data = {"token": self.config_token, "app_id": app_id, "manifest": json.dumps(self.manifest_config)}
         response = requests.post(update_api, post_data)
         self.__checkResponse(response, "changeAppConfig")
+
+    def switchSocketModeAndEventSubsc(self, app_id: str, socket_mode_enabled: bool, request_url: str = "https://XXX-XXX-XXX-XXX/slack/events"):
+        """
+        Socket ModeとEvent Subscriptionを切り替える
+
+        Args:
+            app_id (str): The ID of the app whose configuration you want to export as a manifest.
+            socket_mode_enabled (bool): Socket Modeを有効無効を切り替える。無効時はEvent Subscriptionが有効になる。
+            request_url (str): Event SubscriptionのRequest URL
+        """
+        self.manifest_config["settings"]["event_subscriptions"]["request_url"] = request_url
+        self.manifest_config["settings"]["socket_mode_enabled"] = socket_mode_enabled
+        if socket_mode_enabled is True:
+            self.manifest_config["settings"]["interactivity"] = {"is_enabled": True}
+        else:
+            self.manifest_config["settings"]["interactivity"] = {"is_enabled": False}
+
+        self.changeAppConfig(app_id)
 
     def __checkResponse(self, response, method_name=""):
         """
@@ -104,11 +198,15 @@ class Manifest(object):
         if response.status_code == 200:
             post_result = response.json()
             if post_result["ok"] is False:
-                print(method_name, "エラー!", post_result["error"], "Slackへの反映に失敗しました")
-                print(post_result["errors"])
+                print(method_name, "エラー内容：", post_result["error"], "Slackへの反映に失敗しました。")
+                if post_result["error"] == "invalid_auth":
+                    print("ヒント: Configuration Tokensの期限が切れていないか確認してください。-> https://api.slack.com/apps")
+                    print(".envを更新した場合、Dev ContainerのRebuildが必要な場合があります。")
+                else:
+                    print(post_result["errors"])
                 ret = False
             else:
-                print(method_name, post_result)
+                # print(method_name, post_result)
                 ret = True
         else:
             print(method_name, response.status_code, response.reason)
@@ -124,11 +222,12 @@ if __name__ == "__main__":
     config = ConfigParser()
     config.read("./slack-app-manifest/config.ini", encoding="utf-8")
 
-    token = os.environ.get("SLACK_CONF_TOKEN")
+    slack_conf_refresh_token = os.environ.get("SLACK_CONF_REFRESH_TOKEN")
+    slack_conf_token = os.environ.get("SLACK_CONF_TOKEN")
     app_id = config.get("Slack", "app_id")
     file_path = config.get("Manifest", "file_path")
 
-    manifest = Manifest(token)
+    manifest = Manifest(slack_conf_refresh_token, slack_conf_token)
 
     # ManifestファイルをSlack Applicationからもってきて、PCに保存する
     manifest.getFileFromApp(app_id)
@@ -136,6 +235,7 @@ if __name__ == "__main__":
 
     # PCに保存したManifestファイルをSlack Applicationに反映する
     # manifest.loadFile(file_path)
+    # manifest.switchSocketModeAndEventSubsc(app_id, socket_mode_enabled=False, request_url="https://AAA-AAA-XXX-XXX/slack/events")
     # manifest.changeAppConfig(app_id)
 
     # 新しいSlack Applicationを作成する
